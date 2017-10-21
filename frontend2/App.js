63d293682fb275ce5ece7a53639f0b1e95a76e70
Theodore Liu <theodoretliu@gmail.com>
2017-10-21 12:28:47 -0400

    Adding shit

--- frontend2/App.js
+++ frontend2/App.js
@@ -10,11 +10,13 @@ import {
   StyleSheet,
   Text,
   View,
-  Button
+  Button,
+  DeviceEventEmitter,
+  Alert
 } from 'react-native';
 
 
-import { readTag, writeTag } from 'nfc-react-native';
+import { readTag, writeTag, getTagId } from 'nfc-react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -25,36 +27,114 @@ const instructions = Platform.select({
 
 
 export default class App extends Component<{}> {
-  handleClick() {
-    readTag([{ sector: 5, bloques: [1,2], clave: 'FFFFFFFFFFFF', tipoClave: 'A' },
-        { sector: 6, bloques: [0,1,2], clave: 'FFFFFFFFFFFF', tipoClave: 'A' },
-        { sector: 4, bloques: [0], clave: 'FFFFFFFFFFFF', tipoClave: 'A' }])
-      .then((card) => {
-        console.log(card)
-        // returns Object {lectura: Array[3], card: "A3F813DB"}
-      }).catch((err) => {
-        console.log(err.message)
-      });
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     test: "",
+  //     hasRead: false
+  //   };
+  // }
+
+
+  // render() {
+  //   readTag([{ sector: 5, blocks: [1,2], key: 'FFFFFFFFFFFF', keyType: 'A' },
+  //       { sector: 6, blocks: [0,1,2], key: 'FFFFFFFFFFFF', keyType: 'A' },
+  //       { sector: 4, blocks: [0], key: 'FFFFFFFFFFFF', keyType: 'A' }])
+  //     .then((card) => {
+
+  //       this.setState()
+  //       console.log(card)
+  //       // returns Object {lectura: Array[3], card: "A3F813DB"}
+  //     }).catch((err) => {
+  //       console.log(err.message)
+  //     });
+  //   return (
+  //     <View style={styles.container}>
+  //       <Text style={styles.welcome}>
+  //         Welcome to React {this.state.test}!
+  //       </Text>
+  //       <Text style={styles.instructions}>
+  //         To get started, edit App.js
+  //       </Text>
+  //       <Text style={styles.instructions}>
+  //         {instructions}
+  //       </Text>
+  //     </View>
+  //   );
+  // }
+
+  readTagId() {
+    getTagId();
+      // .then(e => alert(e));
+  }
+
+  readTagData() {
+    readTag([
+      { sector: 1, blocks: [1,2], clave: 'FFFFFFFFFFFF', keyType: 'A' },
+      { sector: 2, blocks: [0,1,2], clave: 'FFFFFFFFFFFF', keyType: 'A' },
+      { sector: 3, blocks: [0], clave: 'FFFFFFFFFFFF', keyType: 'A' }
+    ]);
+  }
+
+  writeTagData() {
+    writeTag([{ sector: 1, blocks: [
+    { index: 1, data: [15,15,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,15,15] },
+    { index: 2, data: [15,15,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,15,15] } ],
+      clave: 'FFFFFFFFFFFF', keyType: 'A' },
+      { sector: 2, blocks: [
+    { index: 0, data: [15,15,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,15,15] },
+    { index: 1, data: [15,15,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,15,15] },
+    { index: 2, data: [15,15,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,15,15] } ],
+      clave: 'FFFFFFFFFFFF', keyType: 'A' },
+    { sector: 3, blocks: [
+    { index: 0, data: [15,15,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,15,15] } ],
+      clave: 'FFFFFFFFFFFF', keyType: 'A' },
+      ], 1148002313)
+  }
+
+  componentDidMount() {
+    DeviceEventEmitter.addListener('onTagError', function (e) {
+        console.log('error', e)
+        alert(JSON.stringify(e))
+    })
+
+    DeviceEventEmitter.addListener('onTagDetected', function (e) {
+        alert(JSON.stringify(e))
+    })
+
+    DeviceEventEmitter.addListener('onTagRead', (e) => {
+        console.log('reading', e)
+        alert(JSON.stringify(e))
+    })
+
+    DeviceEventEmitter.addListener('onTagWrite', (e) => {
+        console.log('writing', e)
+        alert(JSON.stringify(e))
+    })
   }
 
   render() {
-    alert("Hello");
-
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-        <Button title={"hello"} onPress={e => this.handleClick(e)}>Testing</Button>
+        <Button
+          onPress={this.readTagId}
+          title="Get id of Tag"
+        />
+        <Button
+          onPress={this.readTagData}
+          title="Get sectors of a Tag"
+        />
+        <Button
+          onPress={this.writeTagData}
+          title="Write sectors of a Tag"
+        />
       </View>
     );
   }
+
 }
 
 const styles = StyleSheet.create({
