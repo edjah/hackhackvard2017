24f81b3e8d65ead05187c1d8057b1d6c4b1924d9
Sean Sullivan <sean_sullivan@college.harvard.edu>
2017-10-21 12:27:52 -0400

    push

---
+++ frontend3/src/main.js
@@ -0,0 +1,25 @@
+import Expo, { Constants } from 'expo';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import createRootNavigator from './navigations/RootNavigator';
+
+class Main extends Component {
+
+  render() {
+    const Layout = createRootNavigator(true);
+    return (
+      <Layout />
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: Constants.statusBarHeight,
+    paddingBottom: 50
+  },
+});
+
+Expo.registerRootComponent(Main);
---
+++ frontend3/src/screens/UpdateInfo/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react"
+import {
+  Text,
+  ScrollView,
+  StyleSheet,
+  View,
+  Button
+} from "react-native"
+import PropTypes from "prop-types"
+
+export default class UpdateInfo extends Component {
+  static navigationOptions = {
+    title: 'Info',
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <ScrollView style={styles.view}>
+        <Text style={styles.header}>{"Hello World"}</Text>
+      </ScrollView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    height: 10,
+    padding: 20,
+    backgroundColor: "#ababab",
+  },
+}
+
