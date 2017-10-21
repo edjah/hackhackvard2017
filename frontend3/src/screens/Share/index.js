d2bdd9acde1af717cb003e45c8269ff484f4303c
Sean Sullivan <sean_sullivan@college.harvard.edu>
2017-10-21 16:39:20 -0400

    push

--- frontend3/src/screens/Share/index.js
+++ frontend3/src/screens/Share/index.js
@@ -8,6 +8,7 @@ import {
 } from "react-native"
 import PropTypes from "prop-types"
 import QRCode from 'react-native-qrcode-svg';
+import { Permissions } from 'expo';
 import {
   Scanner
 } from '../../components'
@@ -22,16 +23,22 @@ export default class Share extends Component {
   }
 
   onRead = (e) => {
-    alert(e);
+    console.log(e);
+  }
+
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({hasCameraPermission: status === 'granted'});
   }
 
   render() {
     return (
-      <ScrollView style={styles.view}>
-        <Text style={styles.header}>{"Hello World Share"}</Text>
-        <QRCode value="hello world" />
-        <Scanner onRead={this.onRead} />
-      </ScrollView>
+      <View style={styles.view}>
+          <View style={styles.sc}>
+            <Scanner style={styles.cam} onRead={this.onRead} />
+          </View>
+          <QRCode value="hello world" size={300}/>
+      </View>
     )
   }
 }
@@ -39,8 +46,16 @@ export default class Share extends Component {
 const styles = StyleSheet.create({
   view: {
     flex: 1,
-    height: 10,
-    padding: 20,
-    backgroundColor: "#ababab",
+    paddingTop: 10,
+    paddingBottom: 10,
+    flexDirection:"column",
+    justifyContent:"space-between",
+    alignItems:"center",
+  },
+  sc: {
+    alignContent:"center",
+    height: 100,
+    width: 100,
+    backgroundColor: "#00f",
   },
 })
